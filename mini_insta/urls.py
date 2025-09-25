ca1907a761ee4472ec67dc91048eb459b7ceaeff
mdiazgar <mdiazg@bu.edu>
2025-09-25 19:05:32 -0400

    Individual profiles show up now

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.conf import settings
 from .views import ProfileListView
+from .views import ProfileDetailView
 from . import views
 
 app_name = "mini_insta"
 
 urlpatterns = [
     path('', views.ProfileListView.as_view(), name="show_all_profiles"), 
-    
+    path("profile/<int:pk>", ProfileDetailView.as_view(), name="show_profile")
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic import DetailView
 from .models import Profile
 # Create your views here.
 
