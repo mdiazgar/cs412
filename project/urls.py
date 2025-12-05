d3ad8d9f5edf5443162ee913bf8031a09c0fdd35
mdiazgar <mdiazg@bu.edu>
2025-12-05 15:15:54 -0500

    Implemented campaign tracker with CRUD views and performance report

--- project/urls.py
+++ project/urls.py
@@ -8,9 +8,14 @@ urlpatterns = [
     path('channels/<int:pk>/', views.ChannelDetailView.as_view(), name='channel_detail'),
 
     path('campaigns/', views.CampaignListView.as_view(), name='campaign_list'),
-    path('campaigns/<int:pk>/', views.CampaignDetailView.as_view(), name='campaign_detail'),
     path('campaigns/create/', views.CampaignCreateView.as_view(), name='campaign_create'),
+    path('campaigns/<int:pk>/', views.CampaignDetailView.as_view(), name='campaign_detail'),
+    path('campaigns/<int:pk>/edit/', views.CampaignUpdateView.as_view(), name='campaign_update'),
 
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('posts/create/', views.PostCreateView.as_view(), name='post_create'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    
+    path('reports/campaign-performance/', views.campaign_performance_report, name='campaign_performance_report'),
+
 ]
--- project/views.py
+++ project/views.py
@@ -1,7 +1,10 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Channel, Campaign, Post
 from .forms import CampaignForm, PostForm
+from django.shortcuts import render
+from django.db.models import Sum
+from datetime import datetime
 
 
 class ChannelListView(ListView):
@@ -42,6 +45,14 @@ class CampaignCreateView(CreateView):
     def get_success_url(self):
         return reverse_lazy('project:campaign_detail', kwargs={'pk': self.object.pk})
 
+class CampaignUpdateView(UpdateView):
+    model = Campaign
+    form_class = CampaignForm
+    template_name = 'project/campaign_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('project:campaign_detail', kwargs={'pk': self.object.pk})
+
 
 class PostCreateView(CreateView):
     model = Post
@@ -50,3 +61,90 @@ class PostCreateView(CreateView):
 
     def get_success_url(self):
         return reverse_lazy('project:post_detail', kwargs={'pk': self.object.pk})
+    
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'project/post_confirm_delete.html'
+
+    def get_success_url(self):
+        return reverse_lazy('project:campaign_detail', kwargs={'pk': self.object.campaign.pk})
+
+
+def campaign_performance_report(request):
+    from .models import Channel, Campaign  
+
+    channel_id = request.GET.get('channel')
+    start_date_str = request.GET.get('start_date')
+    end_date_str = request.GET.get('end_date')
+
+    campaigns = Campaign.objects.select_related('channel')
+
+    if channel_id:
+        campaigns = campaigns.filter(channel_id=channel_id)
+
+    if start_date_str:
+        try:
+            start_date = datetime.strptime(start_date_str, '%Y-%m-%d').date()
+            campaigns = campaigns.filter(start_date__gte=start_date)
+        except ValueError:
+            start_date = None
+    else:
+        start_date = None
+
+    if end_date_str:
+        try:
+            end_date = datetime.strptime(end_date_str, '%Y-%m-%d').date()
+            campaigns = campaigns.filter(start_date__lte=end_date)
+        except ValueError:
+            end_date = None
+    else:
+        end_date = None
+
+    results = []
+    for campaign in campaigns:
+        posts = campaign.posts.all().select_related('metrics')
+
+        total_impressions = 0
+        total_likes = 0
+        total_comments = 0
+        total_shares = 0
+        total_clicks = 0
+
+        for post in posts:
+            if hasattr(post, 'metrics') and post.metrics:
+                total_impressions += post.metrics.impressions
+                total_likes += post.metrics.likes
+                total_comments += post.metrics.comments
+                total_shares += post.metrics.shares
+                total_clicks += post.metrics.clicks
+
+        if total_impressions > 0:
+            engagement_rate = (total_likes + total_comments + total_shares) / total_impressions
+            ctr = total_clicks / total_impressions
+        else:
+            engagement_rate = 0
+            ctr = 0
+
+        results.append({
+            'campaign': campaign,
+            'total_impressions': total_impressions,
+            'total_likes': total_likes,
+            'total_comments': total_comments,
+            'total_shares': total_shares,
+            'total_clicks': total_clicks,
+            'engagement_rate': engagement_rate,
+            'ctr': ctr,
+        })
+
+    results.sort(key=lambda x: x['total_impressions'], reverse=True)
+
+    channels = Channel.objects.all()
+
+    context = {
+        'channels': channels,
+        'results': results,
+        'selected_channel_id': channel_id or '',
+        'start_date': start_date_str or '',
+        'end_date': end_date_str or '',
+    }
+    return render(request, 'project/report_campaign_performance.html', context)
