2362d14ab9e01acabc0d9b5da3cf9c4043434450
mdiazgar <mdiazg@bu.edu>
2025-12-09 20:11:08 -0500

    Simplify wsgi.py to see if it finally fixes my deployment error

--- cs412/wsgi.py
+++ cs412/wsgi.py
@@ -11,11 +11,11 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
+ROOT_DIR = os.path.dirname(os.path.abspath(os.path.join(__file__, os.pardir)))
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if BASE_DIR not in sys.path:
-    sys.path.insert(0, BASE_DIR)
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cs412.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cs412.settings")
 
 application = get_wsgi_application()
